14b99de1e386ae8ec1d2724d2d0189060d309331
prabanjanr <106054547+prabanjanr@users.noreply.github.com>
2024-09-05 20:11:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="navbar">
         <FaCoins className="icon" />
         <h1>
-          Coin <span className="purple">Search</span>
+          Crypto <span className="purple">Stat</span>
         </h1>
       </div>
     </Link>
